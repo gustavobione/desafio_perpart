cb99370012ebe40f19f21cbdf56eb727742cce68
gustavo_bione <gustavoteixeira394@gmail.com>
2026-04-22 18:39:03 -0300

    feat(frontend): criação da estrutura do app e instalação de todas as dependencias nescessarias, além de organização do global.css e layout

--- desafio_perpart_app/app/layout.tsx
+++ desafio_perpart_app/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { LayoutProvider, UiProvider } from "@uigovpe/components";
+import { Inter } from 'next/font/google';
+import '@uigovpe/styles';
+import './globals.css';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -23,11 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="pt-br">
+      <body className={`${inter.className} antialiased`}>
+        <LayoutProvider>
+          <UiProvider>
+            {children}
+          </UiProvider>
+        </LayoutProvider>
+      </body>
     </html>
   );
 }
